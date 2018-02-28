057c43ae0c21a1be92125c7c5ce3435977a2aa2b
Zuo Yang <zuya@microsoft.com>
2018-02-28 19:58:18 +0800

    Use ADO.NET to query data.

--- Controllers/RequestsController.cs
+++ Controllers/RequestsController.cs
@@ -1,23 +1,57 @@
-using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using CsvToolDb.Models;
 using Microsoft.AspNet.OData;
-using Microsoft.EntityFrameworkCore;
 
 namespace CsvToolDb.Controllers
 {
     public class RequestsController : ODataController
     {
-        private readonly CsvToolContext _context;
+        // private readonly CsvToolContext _context;
 
-        public RequestsController(CsvToolContext context)
-        {
-            _context = context;
-        }
+        // public RequestsController(CsvToolContext context)
+        // {
+        //     _context = context;
+        // }
 
         [EnableQuery]
-        public IQueryable<Request> Get()
+        public IEnumerable<Request> Get()
         {
-            return _context.Requests.AsNoTracking().AsQueryable();
+            // return _context.Requests.AsNoTracking().AsQueryable();
+            List<Request> requests = new List<Request>();
+            
+            using (SqlConnection con = new SqlConnection("Server=(localdb)\\mssqllocaldb;Database=CsvToolDb.TestDb;Trusted_Connection=True;"))
+            {
+                SqlCommand cmd = new SqlCommand("GetRequests", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Request r = new Request();
+                    r.Id = rdr["Id"].ToString();
+                    r.Title = rdr["Title"].ToString();
+                    r.Brand = rdr["Brand"].ToString();
+                    r.Status = rdr["Status"].ToString();
+
+                    DateTime.TryParse(rdr["CreatedDate"].ToString(), out DateTime CreatedDate);
+                    r.CreatedDate = CreatedDate;
+
+                    DateTime.TryParse(rdr["CreatedDate"].ToString(), out DateTime SubmittedDate);
+                    r.SubmittedDate = SubmittedDate;
+
+                    DateTime.TryParse(rdr["CreatedDate"].ToString(), out DateTime TaskSLADate);
+                    r.TaskSLADate = TaskSLADate;
+
+                    r.ReportBack = rdr["TaskSLADate"].ToString();
+
+                    requests.Add(r);
+                }
+                con.Close();
+            }
+            return requests; 
         }
     } 
 }
